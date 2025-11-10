b0b767a86fa84b252494fd74600b5ba2cf542cb7
chapter <chuongninolxag@gmail.com>
2025-11-10 22:41:49 +0700

    feat: add multi middleware example

---
+++ 02-express-tutorial/09-middleware-use.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const { logger } = require("./logger");
+
+// req => middleware => res
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items");
+});
+
+app.listen(5500, () => {
+  console.log("Server is listening on port 5500....");
+});
